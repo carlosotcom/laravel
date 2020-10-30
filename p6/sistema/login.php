820cc885195ab77dd1a065e45fc8a1e2db061769
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-10-30 20:00:52 +0100

    login y principal

---
+++ p6/sistema/login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" type = "text/css" href="css/stylelogin.css">
+</head>
+<body>
+    <section id="container">
+        <form action ="" method="post">
+            <h3>Iniciar sesion</h3>
+         <img src="img/login.png" alt="login">
+        <input type="text" name="usuario" placeholder="Usuario">
+        <input type="password" name="clave" placeholder="Contraseña">
+        <p class="alert"></p>
+        <input type="submit" value="INGRESAR">
+        </form>
+    </section>
+</body>
+</html>
