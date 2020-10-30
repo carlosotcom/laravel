756c5699fca0b1f29fe3b419fc4e0898223bf0ed
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-10-30 21:38:20 +0100

    login a base de datos

--- p6/sistema/login.php
+++ p6/sistema/login.php
@@ -1,3 +1,38 @@
+<?php
+
+$alert = '';
+    if(!empty($_POST))
+    {
+        if(empty($_POST['usuario']) || empty($_POST['clave']))
+        {
+            $alert = "Ingrese su usuario y contraseña";
+        }else{
+            require_once "conexion.php";
+
+            $user = $_POST['usuario'];
+            $pass = $_POST['clave'];
+            $query = mysqli_query($conection, "SELECT * FROM users_admin WHERE username = '$user' AND password = '$pass'");
+            $result = mysql_num_rows($query);
+
+            if($result > 0)
+            {
+                $data = mysqli_fetch_array($query);
+                session_start();
+                $_SESSION['active'] = true;
+                $_SESSION['idusername'] = $data['id_user_admin'];
+                $_SESSION['nombreusuario'] = $data['username'];
+                $_SESSION['nombre'] = $data['name'];
+                $_SESSION['mail'] = $data['email'];
+                $_SESSION['passw'] = $data['password'];
+
+                header('location: sistema/');            
+            }else{
+                $alert = 'El usuario o la clave son incorrectos';
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -13,7 +48,7 @@
          <img src="img/login.png" alt="login">
         <input type="text" name="usuario" placeholder="Usuario">
         <input type="password" name="clave" placeholder="Contraseña">
-        <p class="alert"></p>
+        <div class="alert"><?php  echo isset($alert)? $alert : ''; ?></div>
         <input type="submit" value="INGRESAR">
         </form>
     </section>
