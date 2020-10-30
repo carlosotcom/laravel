53a0c6d20fb08fb559662b04d6f008649eb72dc8
IvanGomezSuarez <59507021+IvanGomezSuarez@users.noreply.github.com>
2020-10-30 22:55:23 +0100

    creada el proyecto como plantilla php

---
+++ p6/sistema/header.php
@@ -0,0 +1,14 @@
+<header>
+		<div class="header">
+			
+			<h1>Centro educativo</h1>
+			<div class="optionsBar">
+				<p>España, 30 Octubre de 2020</p>
+				<span>|</span>
+				<span class="user">Ejemplo usuario</span>
+				<img class="photouser" src="img/user.png" alt="Usuario">
+				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
+			</div>
+		</div>
+        <?php include "nav.php"; ?>
+	</header>
--- p6/sistema/index.php
+++ p6/sistema/index.php
@@ -2,58 +2,19 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<?php include "scripts.php"; ?>
 	<title>Administracion</title>
 </head>
 <body>
-	<header>
-		<div class="header">
-			
-			<h1>Centro educativo</h1>
-			<div class="optionsBar">
-				<p>España, 30 Octubre de 2020</p>
-				<span>|</span>
-				<span class="user">Ejemplo usuario</span>
-				<img class="photouser" src="img/user.png" alt="Usuario">
-				<a href="salir.php"><img class="close" src="img/salir.png" alt="Salir del sistema" title="Salir"></a>
-			</div>
-		</div>
-		<nav>
-			<ul>
-				<li><a href="#">Inicio</a></li>
-				<li class="principal">
-					<a href="#">Usuario administrador</a>
-					<ul>
-						<li><a href="#">Nuevo Usuario administrador</a></li>
-						<li><a href="#">Lista de Usuarios administradores</a></li>
-					</ul>
-				</li>
-				<li class="principal">
-					<a href="#">Profesor</a>
-					<ul>
-						<li><a href="#">Nuevo profesor</a></li>
-						<li><a href="#">Lista de Profesores</a></li>
-					</ul>
-				</li>
-				<li class="principal">
-					<a href="#">Cursos</a>
-					<ul>
-						<li><a href="#">Nuevo Curso</a></li>
-						<li><a href="#">Lista de Cursos</a></li>
-					</ul>
-				</li>
-				<li class="principal">
-					<a href="#">Asignaturas</a>
-					<ul>
-						<li><a href="#">Nueva asignatura</a></li>
-						<li><a href="#">Lista de asignaturas</a></li>
-					</ul>
-				</li>
-			</ul>
-		</nav>
-	</header>
+	<?php include "header.php"; ?>	
 	<section id="container">
 		<h1>Bienvenido al sistema</h1>
 	</section>
+
+
+
+
+
+	<?php include "footer.php"; ?>
 </body>
 </html>
